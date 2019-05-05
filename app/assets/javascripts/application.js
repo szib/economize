72d8f1a6f46e45f7a182ed488770d288d6f4eb6f
Ivan Szebenszki <szib77@gmail.com>
2019-05-05 20:47:50 +0100

    Install Semantic UI

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//
+// Loads all Semantic javascripts
+//= require semantic-ui
+//
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
