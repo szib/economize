46bc54973317d3747e8b2c6f5aec897589f747cb
Ivan Szebenszki <szib77@gmail.com>
2019-05-07 13:49:32 +0100

    Fix: service_id in PriceRecord

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_07_092235) do
+ActiveRecord::Schema.define(version: 2019_05_07_124543) do
 
   create_table "accounts", force: :cascade do |t|
     t.integer "user_id"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2019_05_07_092235) do
     t.datetime "effective_from"
     t.float "monthly_price"
     t.float "joining_fee"
-    t.float "service_id"
+    t.integer "service_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
