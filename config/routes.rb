b29878109d1410ccbdbe57ac7e942afbbb7dace4
Ivan Szebenszki <szib77@gmail.com>
2019-05-09 12:09:40 +0100

    Subscription routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   resources :users
   resources :services
 
+  # ========================================
+  #    SUBSCRIPTIONS
+  # ========================================
   get '/subscriptions/archive', to: 'subscriptions#archive', as: 'archive_subscriptions'
-  resources :subscriptions
+  delete '/subscriptions/:id/cancel', to: 'subscriptions#cancel', as: 'cancel_subscription'
+  resources :subscriptions, only: %i[index show new create]
 
   get '/signup', to: 'users#new'
 
@@ -14,5 +18,4 @@ Rails.application.routes.draw do
   get '/dashboard', to: 'dashboard#index'
 
   root 'welcome#home'
-  delete '/subscriptions/:id/cancel', to: 'subscriptions#cancel', as: 'cancel_subscription'
 end
