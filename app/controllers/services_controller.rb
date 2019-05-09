ebfeb1e155448c36a1798ecfb90d425235b2419b
Ivan Szebenszki <szib77@gmail.com>
2019-05-09 21:38:59 +0100

    Strong params with pricerecords

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -2,7 +2,7 @@ class ServicesController < ApplicationController
   before_action :find_service, except: %i[index new create]
 
   def index
-    @services = Service.all
+    @services = Service.includes(:price_records)
   end
 
   def show
@@ -26,9 +26,10 @@ class ServicesController < ApplicationController
 
   def create
     authorized_for(current_user.id)
-    @service = Service.new(service_params)
+    @service = Service.new(service_params_create)
+    @service.price_records.last.effective_from = DateTime.now
+
     if @service.save
-      @service.current_price = service_params[:current_price]
       flash[:success] = 'Service successfully created'
       redirect_to @service
     else
@@ -39,7 +40,11 @@ class ServicesController < ApplicationController
 
   def update
     authorized_for(current_user.id)
-    if @service.update_attributes(service_params)
+    unless @service.current_price == current_price_params[:monthly_price].to_i
+      @service.price_records.build(current_price_params)
+    end
+
+    if @service.update(service_params_update)
       flash[:success] = 'Service was successfully updated'
       redirect_to @service
     else
@@ -65,8 +70,19 @@ class ServicesController < ApplicationController
     @service = Service.find(params[:id])
   end
 
-  def service_params
-    # params.require(:service).permit(:name, :description, price_records_attributes: %i[monthly_price])
-    params.require(:service).permit(:name, :description, :current_price)
+  def service_params_create
+    params.require(:service).permit(:name, :description, price_records_attributes: %i[monthly_price])
+  end
+
+  def service_params_update
+    params.require(:service).permit(:name, :description)
+  end
+
+  def current_price_params
+    extra = { effective_from: DateTime.now }
+    pr_params = params.require(:service)
+                      .except(:name, :description, :id)
+                      .permit(price_records_attributes: %i[monthly_price])
+    pr_params.values.first['0'].merge(extra)
   end
 end
