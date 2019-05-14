5028c7332a2d13b5cba67a1d4cc299f11039ba09
Ivan Szebenszki <szib77@gmail.com>
2019-05-14 10:48:51 +0100

    Seed with more date

--- db/seeds_with_faker.rb
+++ db/seeds_with_faker.rb
@@ -40,15 +40,15 @@ Service.create(services)
 
 Service.all.each do |service|
   service_start_date = Faker::Date.between(3.years.ago, Date.today)
-  price_record_dates = (1..5).to_a.sample.times.map do
+  price_record_dates = (1..30).to_a.sample.times.map do
     Faker::Date.between(service_start_date, Date.today)
   end.sort.uniq
 
-  baseprice = (5..10).to_a.sample
+  baseprice = (5..100).to_a.sample
   price_record_data = price_record_dates.each_with_index.map do |date, idx|
     {
       effective_from: date,
-      monthly_price: baseprice + idx,
+      monthly_price: baseprice + (baseprice * 0.05 * idx),
       service_id: service.id
     }
   end
@@ -56,20 +56,20 @@ Service.all.each do |service|
 end
 
 # ========================================
-#    SUBSRICTIONS
+#    SUBSCRIPTIONS
 # ========================================
 
 all_service_id = Service.all.map(&:id)
 Account.all.each do |account|
   service_ids = []
-  (0..5).to_a.sample.times { service_ids << all_service_id.sample }
+  (0..20).to_a.sample.times { service_ids << all_service_id.sample }
 
   service_ids.uniq.each do |service_id|
     service = Service.find(service_id)
     oldest_price_record = Service.find(service_id).oldest_price_record
 
     start_date = Faker::Date.between(oldest_price_record.effective_from, Date.today)
-    end_date = if (1..100).to_a.sample >= 70
+    end_date = if (1..100).to_a.sample >= 40
                  nil
                else
                  Faker::Date.between(start_date, Date.today)
